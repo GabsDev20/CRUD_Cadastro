c57c79315b457257b2c0c80177a52d0406cfdf51
Gabriel Henrique <Gabriel Henrique@DESKTOP-LDNFS8O>
2022-07-29 12:01:14 -0300

    Adicionar arquivos de projeto.

---
+++ CRUD_Cadastro/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace CRUD_Cadastro
+{
+    public partial class FormCadastroCliente : Form
+    {
+        string connectionString = @"Server=.;Database=bdcadastro;Trusted_Connection=True;";
+        //Variavel de conexão ao BD
+
+        bool novo;
+        // variavel para cada registro novo
+        public FormCadastroCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tsbNovo.Enabled = true;
+            tsbSalvar.Enabled = false;
+            tsbCancelar.Enabled = false;
+            tsbExcluir.Enabled = false;
+            tsbBuscaID.Enabled = true;
+            tsbBuscar.Enabled = true;
+            txtNome.Enabled = false;
+            txtEndereco.Enabled = false;
+            mskCEP.Enabled = false;
+            txtBairro.Enabled = false;
+            txtCidade.Enabled = false;
+            txtUF.Enabled = false;
+            mskTelefone.Enabled = false;
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void tsbNovo_Click(object sender, EventArgs e)
+        {
+            tsbNovo.Enabled = false;
+            tsbSalvar.Enabled = true;
+            tsbCancelar.Enabled = true;
+            tsbExcluir.Enabled = false;
+            tsbBuscaID.Enabled = false;
+            tsbBuscar.Enabled = false;
+            txtNome.Enabled = true;
+            txtEndereco.Enabled = true;
+            mskCEP.Enabled = true;
+            txtBairro.Enabled = true;
+            txtCidade.Enabled = true;
+            txtUF.Enabled = true;
+            mskTelefone.Enabled = true;
+            txtNome.Focus();
+            novo = true;
+        }
+
+        private void tsbSalvar_Click(object sender, EventArgs e)
+        {
+            if (novo)
+            {
+                string sql = "INSERT INTO CLIENTE (NOME,ENDERECO,CEP,BAIRRO,CIDADE,UF,TELEFONE) " +
+     "VALUES (@Nome, @Endereco, @Cep, @Bairro, @Cidade, @Uf, @Telefone)";
+
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Nome", txtNome.Text);
+                cmd.Parameters.AddWithValue("@Endereco", txtEndereco.Text);
+                cmd.Parameters.AddWithValue("@Cep", mskCEP.Text);
+                cmd.Parameters.AddWithValue("@Bairro", txtBairro.Text);
+                cmd.Parameters.AddWithValue("@Cidade", txtCidade.Text);
+                cmd.Parameters.AddWithValue("@Uf", txtUF.Text);
+                cmd.Parameters.AddWithValue("@Telefone", mskTelefone.Text);
+                cmd.CommandType = CommandType.Text;
+                con.Open();
+
+                try
+                {
+                    int i = cmd.ExecuteNonQuery();
+                    if (i > 0)
+                        MessageBox.Show("Registro incluido com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro: " + ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            else
+            {
+                string sql = "UPDATE CLIENTE SET NOME=@Nome, ENDERECO=@Endereco,CEP = @Cep, BAIRRO = @Bairro, " +
+              "CIDADE=@Cidade, UF=@Uf, TELEFONE=@Telefone WHERE ID=@Id";
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Id", txtID.Text);
+                cmd.Parameters.AddWithValue("@Nome", txtNome.Text);
+                cmd.Parameters.AddWithValue("@Endereco", txtEndereco.Text);
+                cmd.Parameters.AddWithValue("@Cep", mskCEP.Text);
+                cmd.Parameters.AddWithValue("@Bairro", txtBairro.Text);
+                cmd.Parameters.AddWithValue("@Cidade", txtCidade.Text);
+                cmd.Parameters.AddWithValue("@Uf", txtUF.Text);
+                cmd.Parameters.AddWithValue("@Telefone", mskTelefone.Text);
+                cmd.CommandType = CommandType.Text;
+                con.Open();
+                try
+                {
+                    int i = cmd.ExecuteNonQuery();
+                    if (i > 0)
+                        MessageBox.Show("Registro atualizado com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro: " + ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            tsbNovo.Enabled = true;
+            tsbSalvar.Enabled = false;
+            tsbCancelar.Enabled = false;
+            tsbExcluir.Enabled = false;
+            tsbBuscaID.Enabled = true;
+            tsbBuscar.Enabled = true;
+            txtNome.Enabled = false;
+            txtEndereco.Enabled = false;
+            mskCEP.Enabled = false;
+            txtBairro.Enabled = false;
+            txtCidade.Enabled = false;
+            txtUF.Enabled = false;
+            mskTelefone.Enabled = false;
+            txtID.Text = "";
+            txtNome.Text = "";
+            txtEndereco.Text = "";
+            mskCEP.Text = "";
+            txtBairro.Text = "";
+            txtCidade.Text = "";
+            txtUF.Text = "";
+            mskTelefone.Text = "";
+        }
+
+        private void tsbCancelar_Click(object sender, EventArgs e)
+        {
+            tsbNovo.Enabled = true;
+            tsbSalvar.Enabled = false;
+            tsbCancelar.Enabled = false;
+            tsbExcluir.Enabled = false;
+            tsbBuscaID.Enabled = true;
+            tsbBuscar.Enabled = true;
+            txtNome.Enabled = false;
+            txtEndereco.Enabled = false;
+            mskCEP.Enabled = false;
+            txtBairro.Enabled = false;
+            txtCidade.Enabled = false;
+            txtUF.Enabled = false;
+            mskTelefone.Enabled = false;
+            txtID.Text = "";
+            txtNome.Text = "";
+            txtEndereco.Text = "";
+            mskCEP.Text = "";
+            txtBairro.Text = "";
+            txtCidade.Text = "";
+            txtUF.Text = "";
+            mskTelefone.Text = "";
+        }
+
+        private void tsbExcluir_Click(object sender, EventArgs e)
+        {
+            string sql = "DELETE FROM CLIENTE WHERE ID=@Id";
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@Id", txtID.Text);
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+
+            try
+            {
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                    MessageBox.Show("Registro excluído com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            tsbNovo.Enabled = true;
+            tsbSalvar.Enabled = false;
+            tsbCancelar.Enabled = false;
+            tsbExcluir.Enabled = false;
+            tsbBuscaID.Enabled = true;
+            tsbBuscar.Enabled = true;
+            txtNome.Enabled = false;
+            txtEndereco.Enabled = false;
+            mskCEP.Enabled = false;
+            txtBairro.Enabled = false;
+            txtCidade.Enabled = false;
+            txtUF.Enabled = false;
+            mskTelefone.Enabled = false;
+            txtID.Text = "";
+            txtNome.Text = "";
+            txtEndereco.Text = "";
+            mskCEP.Text = "";
+            txtBairro.Text = "";
+            txtCidade.Text = "";
+            txtUF.Text = "";
+            mskTelefone.Text = "";
+        }
+
+        private void tsbBuscar_Click(object sender, EventArgs e)
+        {
+            string sql = "SELECT * FROM CLIENTE WHERE ID=@Id";
+
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.Parameters.AddWithValue("@Id", tsbBuscaID.Text);
+            cmd.CommandType = CommandType.Text;
+            SqlDataReader reader;
+            con.Open();
+            try
+            {
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    tsbNovo.Enabled = false;
+                    tsbSalvar.Enabled = true;
+                    tsbCancelar.Enabled = true;
+                    tsbExcluir.Enabled = true;
+                    tsbBuscaID.Enabled = false;
+                    tsbBuscar.Enabled = false;
+                    txtNome.Enabled = true;
+                    txtEndereco.Enabled = true;
+                    mskCEP.Enabled = true;
+                    txtBairro.Enabled = true;
+                    txtCidade.Enabled = true;
+                    txtUF.Enabled = true;
+                    mskTelefone.Enabled = true;
+                    txtNome.Focus();
+                    txtID.Text = reader[0].ToString();
+                    txtNome.Text = reader[1].ToString();
+                    txtEndereco.Text = reader[2].ToString();
+                    mskCEP.Text = reader[3].ToString();
+                    txtBairro.Text = reader[4].ToString();
+                    txtCidade.Text = reader[5].ToString();
+                    txtUF.Text = reader[6].ToString();
+                    mskTelefone.Text = reader[7].ToString();
+                    novo = false;
+                }
+                else
+                    MessageBox.Show("Nenhum registro encontrado com o Id informado!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.ToString());
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            tsbBuscaID.Text = "";
+        }
+    }
+}
